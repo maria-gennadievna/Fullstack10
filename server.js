78b9801547d8fa828377a0299894227a1cca116d
Joker <114473100+maria-gennadievna@users.noreply.github.com>
2024-03-27 14:56:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fetch = require('node-fetch');
 
 app.use('/', express.static('public'))
 
-app.get('/api/weather', (req, res) => {
-    //   res.send('Hello World!')
+app.get('/api/weather', async (req, res) => {
+    const APIkey = 'a2906b093bfe0cb70f7c5e3e7b3baeb7'
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${61.77816}&lon=${34.36404}&appid=${APIkey}`);
+    const data = await response.json();
+
     res.send({
-        'city': data.name,
-        'temp': Math.round(data.main.temp) + "°C",
-        'humidity': data.main.humidity + "%",
-        'wind': data.wind.speed + " Km/h"
+         'name': data.name,
+         'temp': Math.round(data.main.temp),
+         'humidity': data.main.humidity  
     })
+
 })
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`)
